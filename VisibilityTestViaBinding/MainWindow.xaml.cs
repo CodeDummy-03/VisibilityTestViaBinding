92b29bb0dda9fcd0c16a958660add025a8accf12
CodeDummy-03 <sohampal1995@gmail.com>
2022-09-21 19:36:26 +0530

    Add files via upload

---
+++ VisibilityTestViaBinding/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VisibilityTestViaBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int count = 2;
+        ViewModel ob = new ViewModel();
+        public MainWindow()
+        {
+            this.DataContext = ob;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(count%2==0)
+                ob.TextblockVisibility = Visibility.Collapsed;
+            else
+                ob.TextblockVisibility = Visibility.Visible;
+            count++;
+        }
+    }
+}
---
+++ VisibilityTestViaBinding/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VisibilityTestViaBinding
+{
+    public class ViewModel:INotifyPropertyChanged
+    {
+        Visibility textblockVisibility;
+        public Visibility TextblockVisibility
+        {
+            get { return textblockVisibility; }
+            set {
+                textblockVisibility = value;
+                NotifyPropertyChanged("TextblockVisibility");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+    }
+}
